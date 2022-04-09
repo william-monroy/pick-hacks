2b4085ec0a04e29c252a7c6ae1b5c909cd604be9
SciDroid <scidroidgames@gmail.com>
2022-04-09 02:53:33 +0000

    feat: Add magicc.link auth

---
+++ pages/auth.js
@@ -0,0 +1,32 @@
+import { useRouter } from 'next/router';
+import { signIn } from 'next-auth/client';
+import { Magic } from 'magic-sdk';
+import { useForm } from 'react-hook-form';
+
+const magic = typeof window !== 'undefined' && new Magic(process.env.NEXT_PUBLIC_MAGIC_PK || 'a');
+
+const Auth = () => {
+    const router = useRouter();
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = async ({ email }) => {
+        if (!magic) throw new Error(`magic not defined`);
+
+        const didToken = await magic.auth.loginWithMagicLink({ email });
+
+        await signIn('credentials', {
+            didToken,
+            callbackUrl: router.query['callbackUrl'],
+        });
+    };
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <input {...register('email', { required: true })} placeholder="nick@example.com" />
+
+            <button type="submit">Sign in</button>
+        </form>
+    );
+}
+
+export default Auth;
